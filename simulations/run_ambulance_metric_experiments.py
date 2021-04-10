Updating resource allocation code, fixing bug in plots, normalizing features

simulations/run_ambulance_metric_experiments.py
@@ -24,14 +24,14 @@ from joblib import Parallel, delayed
 
 DEFAULT_CONFIG =  or_suite.envs.env_configs.ambulance_metric_default_config
 epLen = DEFAULT_CONFIG['epLen']
-nEps = 5
-numIters = 2
+nEps = 1000
+numIters = 20
 
 epsilon = (nEps * epLen)**(-1 / 4)
 action_net = np.arange(start=0, stop=1, step=epsilon)
 state_net = np.arange(start=0, stop=1, step=epsilon)
 
-scaling = 0.5
+scaling_list = [.001, 0.01, 0.1, 0.5, 1., 2.]
 
 
 
@@ -60,7 +60,7 @@ def beta(step):
 # arrival_dists = [shifting, uniform, beta]
 arrival_dists = [beta]
 # num_ambulances = [1,3]
-num_ambulances = [3]
+num_ambulances = [1, 3]
 # alphas = [0, 0.25, 1]
 alphas = [0]
 
@@ -77,14 +77,14 @@ for num_ambulance in num_ambulances:
             CONFIG['starting_state'] = np.array([0 for _ in range(num_ambulance)])
             ambulance_env = gym.make('Ambulance-v0', config=CONFIG)
             mon_env = Monitor(ambulance_env)
-            agents = {# 'SB PPO': PPO(MlpPolicy, mon_env, gamma=1, verbose=0, n_steps=epLen),
-            # 'Random': or_suite.agents.rl.random.randomAgent(),
-            # 'Stable': or_suite.agents.ambulance.stable.stableAgent(DEFAULT_CONFIG['epLen']),
-            # 'Median': or_suite.agents.ambulance.median.medianAgent(DEFAULT_CONFIG['epLen'])
-            'AdaQL': or_suite.agents.rl.adaptive_Agent.AdaptiveDiscretization(epLen, scaling, 6),
-            # 'AdaMB': or_suite.agents.rl.adaptive_model_Agent.AdaptiveModelBasedDiscretization(epLen, numIters, scaling, 0, 2, True, True),
-            # 'Unif QL': or_suite.agents.rl.eNet_Multiple.eNet(action_net, state_net, epLen, scaling, (num_ambulance,num_ambulance)),
-            # 'Unif MB': or_suite.agents.rl.eNet_model_Agent_Multiple.eNetModelBased(action_net, state_net, epLen, scaling, (num_ambulance,num_ambulance), 0, False)
+            agents = {'SB PPO': PPO(MlpPolicy, mon_env, gamma=1, verbose=0, n_steps=epLen),
+            'Random': or_suite.agents.rl.random.randomAgent(),
+            'Stable': or_suite.agents.ambulance.stable.stableAgent(DEFAULT_CONFIG['epLen']),
+            'Median': or_suite.agents.ambulance.median.medianAgent(DEFAULT_CONFIG['epLen']),
+            'AdaQL': or_suite.agents.rl.adaptive_Agent.AdaptiveDiscretization(epLen, scaling_list[0], True, num_ambulance*2),
+            'AdaMB': or_suite.agents.rl.adaptive_model_Agent.AdaptiveModelBasedDiscretization(epLen, scaling_list[0], 0, 2, True, True, num_ambulance, num_ambulance),
+            'Unif QL': or_suite.agents.rl.eNet_Multiple.eNet(action_net, state_net, epLen, scaling_list[0], (num_ambulance,num_ambulance)),
+            'Unif MB': or_suite.agents.rl.eNet_model_Agent_Multiple.eNetModelBased(action_net, state_net, epLen, scaling_list[0], (num_ambulance,num_ambulance), 0, False)
             }
 
 
@@ -93,6 +93,8 @@ for num_ambulance in num_ambulances:
                 DEFAULT_SETTINGS['dirPath'] = '../data/ambulance_metric_'+str(agent)+'_'+str(num_ambulance)+'_'+str(alpha)+'_'+str(arrival_dist.__name__)+'/'
                 if agent == 'SB PPO':
                     or_suite.utils.run_single_sb_algo(mon_env, agents[agent], DEFAULT_SETTINGS)
+                elif agent == 'AdaQL' or agent == 'Unif QL' or agent == 'AdaMB' or agent == 'Unif MB':
+                    or_suite.utils.run_single_algo_tune(ambulance_env, agents[agent], scaling_list, DEFAULT_SETTINGS)
                 else:
                     or_suite.utils.run_single_algo(ambulance_env, agents[agent], DEFAULT_SETTINGS)
 
@@ -112,7 +114,7 @@ for num_ambulance in num_ambulances:
 #             for agent in agents:
 #                 if num_ambulance > 1 and (agent == 'AdaQL' or agent == 'AdaMB'):
 #                     continue
-#                 path_list_line.append('../data/ambulance_metric_'+str(agent)+'_'+str(num_ambulance)+'_'+str(alpha)+'_'+str(arrival_dist.__name__)+'/data.csv')
+#                 path_list_line.append('../data/ambulance_metric_'+str(agent)+'_'+str(num_ambulance)+'_'+str(alpha)+'_'+str(arrival_dist.__name__))
 #                 algo_list_line.append(str(agent))
 #                 if agent != 'SB PPO':
 #                     path_list_radar.append('../data/ambulance_metric_'+str(agent)+'_'+str(num_ambulance)+'_'+str(alpha)+'_'+str(arrival_dist.__name__))