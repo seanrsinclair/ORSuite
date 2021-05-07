ff240fc64951b93c4364c67429a3b73ce7c1d71f
Mays Cortez <lovatic27@gmail.com>
2021-05-07 14:33:33 -0400

    updated default params

--- or_suite/envs/vaccine_allotment/testing_dynamics_vaccine.py
+++ or_suite/envs/vaccine_allotment/testing_dynamics_vaccine.py
@@ -18,11 +18,11 @@ default_parameters1 = {'contact_matrix': np.array([[0.0001, 0.0001, 0.00003, 0.0
                                                    [0, 0, 0, 0.00003, 0, 0]]),
                        'P': np.array([0.15, 0.15, 0.7, 0.2]),
                        'H': np.array([0.2, 0.2, 0.8, 0.3]),
-                       'beta': 0,
-                       'gamma': 0,
-                       'vaccines': 0,
-                       'priority': [],
-                       'time_step': 0}
+                       'beta': 1/7,
+                       'gamma': 100,
+                       'vaccines': 500,
+                       'priority': ["1", "2", "3", "4"],
+                       'time_step': 7}
 starting_state1 = np.array([1090, 2490, 990, 5390, 10, 10, 10, 10, 0, 0, 0])
 
 # default parameters 2
@@ -32,31 +32,30 @@ default_parameters2 = {'contact_matrix': np.array([[0.0001, 0.0001, 0.00003, 0.0
                                                    [0, 0, 0, 0.00003, 0, 0]]),
                        'P': np.array([0.15, 0.15, 0.7, 0.2]),
                        'H': np.array([0.2, 0.2, 0.8, 0.3]),
-                       'beta': 0,
-                       'gamma': 0,
-                       'vaccines': 0,
+                       'beta': 1/7,
+                       'gamma': 100,
+                       'vaccines': 500,
                        'priority': [],
-                       'time_step': 0}
+                       'time_step': 1}
 starting_state2 = np.array([1090, 2490, 990, 5390, 10, 10, 10, 10, 0, 0, 0])
 
 # default parameters 3
-default_parameters1 = {'contact_matrix': np.array([[0.0001, 0.0001, 0.00003, 0.00003, 0, 0.0001],
+default_parameters3 = {'contact_matrix': np.array([[0.0001, 0.0001, 0.00003, 0.00003, 0, 0.0001],
                                                    [0, 0.0001, 0.00005, 0.0001, 0, 0],
                                                    [0, 0, 0.00003, 0.00003, 0, 0],
                                                    [0, 0, 0, 0.00003, 0, 0]]),
                        'P': np.array([0.15, 0.15, 0.7, 0.2]),
                        'H': np.array([0.2, 0.2, 0.8, 0.3]),
                        'beta': 0,
-                       'gamma': 0,
-                       'vaccines': 0,
-                       'priority': [],
-                       'time_step': 0}
+                       'gamma': 100,
+                       'vaccines': 500,
+                       'priority': ["1", "2", "3", "4"],
+                       'time_step': 1}
 starting_state3 = np.array([990, 1990, 990, 5990, 10, 10, 10, 10, 0, 0, 0])
 
 np.random.seed(master_seed)
-newState, info = dynamics_model(params=default_parameters1, pop=starting_state1)
-
-#print(info.keys())
+newState, info = dynamics_model(params=default_parameters1, population=starting_state1)
+print(info.keys())
 
 vaccine_4groups_default_config = {'epLen': 4, 
     'starting_state': np.array([990, 1990, 990, 5990, 10, 10, 10, 10, 0, 0, 0]), 
