f2ff2d9b4aee460020e34c02845be8b2deb1ea11
Sean Sinclair <seanrsinclair@gmail.com>
2021-05-18 09:42:36 -0400

    update setup and readme file

--- setup.py
+++ setup.py
@@ -1,20 +1,68 @@
 #!usr/bin/env python
 
+
+
+
 from setuptools import setup, find_packages
 import sys
 import os
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'or_suite'))
-from or_suite.version import VERSION
 
-setup(name='or-suite',
-	version=VERSION,
+packages = find_packages(exclude=['docs', 'notebooks', 'assets'])
+
+install_requires = [
+	'gym',
+	'numpy>=1.17',
+	'pandas',
+	'networkx',
+	'scipy',
+	'matplotlib',
+	'seaborn',
+	'scikit-learn',
+	'scikit-learn-extra',
+	'stable-baselines3',
+	'cvxpy',
+	'pyglet==1.5.16',
+    'joblib',
+]
+
+tests_require = [
+]
+
+full_requires = [
+]
+
+extras_require = {
+    'full': full_requires,
+    'test': tests_require,
+}
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='or-suite',
+	version='0.0.1',
 	description='OR-Suite: A set of environments for developing reinforcement learning agents for OR problems.',
+	long_descrption=long_description,
+	long_description_content_type='text/markdown',
 	author='Christopher Archer, Siddhartha Banerjee, Shashank Pathak, Carrie Rucker, Sean Sinclair, Christina Yu',
 	author_email = 'srs429@cornell.edu',
 	license='MIT',
 	url='https://github.com/seanrsinclair/ORSuite',
-	packages = find_packages(),
-	zip_safe = False,
-	python_requires = '>=3.6'
+    packages=packages,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require=extras_require,
+    zip_safe=False,
 )
